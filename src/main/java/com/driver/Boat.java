1de1960b227d78e9d8ed1fbd5aebde0277205517
shiva-saini <shivasainisiyana2211@gmail.com>
2023-02-11 19:22:54 +0530

    Done11

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,9 +4,7 @@ public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
 
-    public Boat(String name,int capacity){
-        this.name = name;
-        this.capacity = capacity;
+    public Boat(){
     }
     public String getName() {
         return name;
